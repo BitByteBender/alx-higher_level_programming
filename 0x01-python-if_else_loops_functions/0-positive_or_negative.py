e8894e33e4ea89555b882b8a1c38030931ee6044
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-02 13:51:01 -0800

    fixed pycodestyle requirement

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
+
 if (number != 0):
- if (number > 0):
-  print(f"{number} is positive")
- else:
-  print(f"{number} is negative")
+    if (number > 0):
+        print(f"{number} is positive")
+    else:
+        print(f"{number} is negative")
 else:
- print(f"{number} is zero")
+    print(f"{number} is zero")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,11 +8,11 @@ txt_greater = "Last digit of {} is {} and is greater than 5"
 last_digit = abs(number) % 10
 
 if (number < 0):
- last_digit = -last_digit
+    last_digit = -last_digit
 
 if (last_digit > 5):
- print(txt_greater.format(number, last_digit))
+    print(txt_greater.format(number, last_digit))
 elif (last_digit == 0):
- print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("Last digit of {} is {} and is 0".format(number, last_digit))
 else:
- print(txt_less.format(number, last_digit))
+    print(txt_less.format(number, last_digit))
