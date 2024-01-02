0a0e41b1f6a5b96d497bcc51f95a85db94fff31f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-02 13:37:30 -0800

    Task 1 - last digit or jedi

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-if (number >= 0):
- last_digit = str(number)[-1]
+
+txt_less = "last digit of {} is {} and is less than 6 and not 0"
+txt_greater = "last digit of {} is {} and is greater than 5"
+last_digit = abs(number) % 10
+
+if (number < 0):
+ last_digit = -last_digit
+
+if (last_digit > 5):
+ print(txt_greater.format(number, last_digit))
+elif (last_digit == 0):
+ print("last digit of {} is {} and not 0".format(number, last_digit))
 else:
- last_digit = str(number)[0] + str(number)[-1]
-
-if (last_digit == "0" or last_digit == "-0"):
- last_digit = "0"
-
-if (last_digit > "5"):
- str = "greater than 5"
-elif (last_digit == "0"):
- str = "0"
-elif (last_digit < "6" and last_digit != "0"):
- str = "less than 6 and not 0"
-print(f"last digit of {number:d} is {last_digit} and is {str}")
+ print(txt_less.format(number, last_digit))
