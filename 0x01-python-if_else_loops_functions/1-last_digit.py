38d85a6d0b0975e475175f6b7d1c9fb59e12cc34
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-02 13:41:11 -0800

    Task 1 - last digit or jedi

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,6 +13,6 @@ if (number < 0):
 if (last_digit > 5):
  print(txt_greater.format(number, last_digit))
 elif (last_digit == 0):
- print("Last digit of {} is {} and not 0".format(number, last_digit))
+ print("Last digit of {} is {} and is 0".format(number, last_digit))
 else:
  print(txt_less.format(number, last_digit))
