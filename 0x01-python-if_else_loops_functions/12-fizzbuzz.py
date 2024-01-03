006e9bb01a650343989ee21363a5c04d596996e1
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-02 18:13:01 -0800

    Task 12 - fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0 and i % 5 == 0):
-            print("FizzBuzz", end=", ")
+            print("FizzBuzz", end=' ')
         elif (i % 3 == 0):
-            print("Fizz", end=", ")
+            print("Fizz", end=' ')
         elif (i % 5 == 0):
-            print("Buzz", end=", " if (i < 100) else '')
+            print("Buzz", end=' ')
         else:
-            print(i, end=", " if (i < 100) else '')
+            print(i, end=' ')
