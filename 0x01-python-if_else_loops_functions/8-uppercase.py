667bca34864f5dc09d98136bdc2ac63a12f492ab
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-02 17:24:34 -0800

    Task 8 - To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     copiedStr = ""
     for alphabet in str:
         if (97 <= ord(alphabet) <= 122):
-            copiedStr += chr(ord(alphabet) - 32)
+            copiedStr += "{}".format(chr(ord(alphabet) - 32))
         else:
             copiedStr += alphabet
     print(copiedStr)
