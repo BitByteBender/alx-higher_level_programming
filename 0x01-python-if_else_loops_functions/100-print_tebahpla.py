35d6f8bcfa92b32722fe564ba44dfbfa5a7306df
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-03 10:15:39 -0800

    Task 14 - Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -5,7 +5,7 @@ def print_chars(begin, end, move):
         char = chr(i + ord('A'))
         if (i % 2 == 1):
             char = char.lower()
-        print(char, end='')
+        print("{}".format(char), end='')
 
 
 print_chars(25, -1, -1)
