db5eb248bc53427b02c3aa2ff2d0576f64196b8a
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-06 11:19:47 -0800

    Task 10 - Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return (None)
+
+    result = []
+    for i in range(len(my_list)):
+        if ((my_list[i] % 2) == 0):
+            result.append(True)
+        else:
+            result.append(False)
+    return (result)
