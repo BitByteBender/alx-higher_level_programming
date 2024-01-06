92d873a662eae75a0bd1cb8244e248fa7e0d39a0
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-06 08:21:09 -0800

    Task 0 - req test

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -9,6 +9,6 @@ def print_list_integer(my_list=[]):
     if (i == len(my_list)):
         return
     else:
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
         i += 1
         print_list_integer(my_list)
