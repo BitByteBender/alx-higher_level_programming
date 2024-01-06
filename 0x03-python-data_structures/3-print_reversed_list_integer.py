40982db61f1eaa40409a3ce64395af5809c0fad1
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-06 08:40:38 -0800

    Task 3 - added a condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,9 @@ i = 0
 
 def print_reversed_list_integer(my_list=[]):
     global i
-    if (i < len(my_list)):
+    if (i == len(my_list)):
+        return
+    else:
         print("{:d}".format(my_list[len(my_list) - i - 1]))
         i += 1
         print_reversed_list_integer(my_list)
