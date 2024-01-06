383c75c50e14bae346e2d0ef3556947f1940cca9
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-06 08:56:17 -0800

    Task 6 - req test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,12 +6,11 @@ i = 0
 def print_matrix_integer(matrix=[[]]):
     global i
     if not matrix[0]:
-        print()
-        return
+        return print()
 
     if (i < len(matrix)):
         for j in range(0, len(matrix[i])):
             cond = ' ' if (j < len(matrix[i]) - 1) else '\n'
-            print("{}".format(matrix[i][j]), end=cond)
+            print("{:d}".format(matrix[i][j]), end=cond)
         i += 1
         print_matrix_integer(matrix)
