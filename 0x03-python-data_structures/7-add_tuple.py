ee2fa641625daf71978c4e4ef7d203aec35e6e3b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-06 10:23:56 -0800

    Task 7 - tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,17 +2,8 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """
-    if (len(tuple_a) < 2):
-        tuple_a += (0, )
-    elif (len(tuple_b) < 2):
-        tuple_b += (0, )
-
-    maxLength = max(len(tuple_b), len(tuple_a))
-    """
     result = ()
-
-    for i in range(0, len(tuple_a)):
+    for i in range(max(len(tuple_a), len(tuple_b))):
         newTuple_a = tuple_a[i] if (len(tuple_a) > i) else 0
         newTuple_b = tuple_b[i] if (len(tuple_b) > i) else 0
         result += (newTuple_a + newTuple_b, )
