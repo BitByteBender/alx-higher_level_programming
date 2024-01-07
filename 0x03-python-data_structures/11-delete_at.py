51bc073d4ad5746639945acea5fe5bfc95655c9b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-07 10:42:06 -0800

    Task 11 - Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,8 +5,5 @@ def delete_at(my_list=[], idx=0):
     if (idx < 0 or idx > (len(my_list) - 1)):
         return (my_list)
     else:
-        for i in range(0, len(my_list)):
-            if (i == idx):
-                continue
-            copiedStr.append(my_list[i])
-    return (copiedStr)
+        del (my_list[idx])
+        return (my_list)
