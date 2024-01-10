d061f728737b3ba0d4aea6cac4c95b5775756e2e
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-10 07:46:02 -0800

    Task 1 - Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,10 +3,7 @@
 
 def search_replace(my_list, search, replace):
     newList = my_list.copy()
-    if not my_list:
-        return (None)
-    else:
-        for i in range(len(my_list)):
-            if (newList[i] == search):
-                newList[i] = replace
+    for i in range(len(my_list)):
+        if (newList[i] == search):
+            newList[i] = replace
     return (newList)
