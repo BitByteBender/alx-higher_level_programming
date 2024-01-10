c41a4c5f4331fe7ef37bc7bb6929ac62b67c5b01
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-10 06:48:53 -0800

    Task 3 - Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    new_set = set()
+    if not set_1 or not set_2:
+        return (None)
+    else:
+        for i in set_1:
+            if i in set_2:
+                new_set.add(i)
+    return (new_set)
