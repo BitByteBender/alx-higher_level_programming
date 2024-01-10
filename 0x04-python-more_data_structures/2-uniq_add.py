dfff810eb6f20724689b2fb8593238bd2d367461
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-10 06:29:15 -0800

    Task 2 - Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    sum = 0
+    if not my_list:
+        return (None)
+    else:
+        for i in range(len(my_list)):
+            isUniq = True
+            for j in range(0, i):
+                if (my_list[i] == my_list[j]):
+                    isUniq = False
+                    break
+            if (isUniq):
+                sum += my_list[i]
+    return (sum)
