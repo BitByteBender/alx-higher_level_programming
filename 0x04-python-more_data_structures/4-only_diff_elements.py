9d459674225a3ea17231156247dc1dcf202332bc
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-10 07:40:51 -0800

    Task 4 - Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,14 +2,11 @@
 
 def only_diff_elements(set_1, set_2):
     new_set = set()
-    if not set_1 or not set_2:
-        return (None)
-    else:
-        for i in set_1:
-            if i not in set_2:
-                new_set.add(i)
+    for i in set_1:
+        if i not in set_2:
+            new_set.add(i)
 
-        for j in set_2:
-            if j not in set_1:
-                new_set.add(j)
+    for j in set_2:
+        if j not in set_1:
+            new_set.add(j)
     return (new_set)
