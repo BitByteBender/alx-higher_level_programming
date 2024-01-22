f6ffdd2d0b91c07e9b5de0c05cb13fcb7f47ffe8
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-22 03:27:44 -0800

    Task 1 - Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end='')
+            counter += 1
+    except IndexError:
+        None
+    print()
+    return (counter)
