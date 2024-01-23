e08b287e3b819d9a7b2860bda133caef8d8bd2a0
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-23 04:25:58 -0800

    Task 2 - size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""Square class"""
+
 
 class Square:
-
+    """Square class with a private instance attribute"""
     def __init__(self, size=0):
-
+        """
+        Square class constructor
+        Args:
+            size: integer square size
+        Note:
+            checks if size is an integer
+            checks if size is greater than or equal 0
+            sets the size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif (size < 0):
