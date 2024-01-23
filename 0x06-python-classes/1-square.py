5b2d8691ec864c241d303386f8b6840ac010ad0f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-23 04:17:32 -0800

    Task 1 - Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""Square class"""
+
 
 class Square:
-
+    """Private instance attribute"""
     def __init__(self, size=0):
+        """
+        Square class constructor
+        Args:
+            size: integer square size
+        """
         self.__size = size
