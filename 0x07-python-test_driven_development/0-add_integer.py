2fe7654bfd2cbc7062cdbc7b97d9b294f19a6492
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-29 04:15:22 -0800

    Tast 0 - Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""Add integer function"""
+
 
 def add_integer(a, b=98):
+    """
+    add_integer - sum of to integers
+    Args:
+        a: first int to be added
+        b: second int to be added
+
+    Raises:
+        TypeError as TE:  if either a or b are not (int/float)
+
+    Returns:
+        either the sum of a and b or TypeError
+    """
     try:
         if not isinstance(a, (int, float)):
             raise TypeError("a must be an integer")
@@ -10,6 +24,7 @@ def add_integer(a, b=98):
             return (int(a) + int(b))
     except TypeError as TE:
         return (TE)
+
 
 if (__name__ == "__main__"):
     import doctest
