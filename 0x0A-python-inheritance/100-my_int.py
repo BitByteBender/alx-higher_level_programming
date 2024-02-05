495639642bbec32106a1fa189cb06c5ce63bec66
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-02-05 13:03:52 -0800

    Task 100 - My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" MyInt class """
+
+
+class MyInt(int):
+    def __eq__(self, int_val):
+        """
+        Overides equality operator
+        Args:
+            int_val: obj to be compared
+        Returns:
+            True if real value of MyInt instance
+            is not equal to int_val
+            otherwise False
+        """
+        return (self.real != int_val)
+
+    def __ne__(self, int_val):
+        """
+        Overides inequality operator
+
+        Args:
+            int_val: obj to be compared
+
+        Returns:
+            True if real value of MyInt instance
+            is equal to int_val
+            otherwise False
+        """
+        return (self.real == int_val)
