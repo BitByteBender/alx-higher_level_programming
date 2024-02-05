bb3ba80946604cc735ba92aaffa856f43d4bf29c
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-02-05 11:17:01 -0800

    Task 6 - Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" BaseGeometry class """
+
+
+class BaseGeometry:
+    """ Public instance method """
+    def area(self):
+        """
+        Method that raises an exception
+        """
+        raise Exception("area() is not implemented")
