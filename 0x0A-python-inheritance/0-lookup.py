b85b52164c06889d298117d5e542c660b4f20418
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-02-05 08:55:40 -0800

    Task 0 - lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,11 @@
 def lookup(obj):
     """
     Returns list of available attrs, methods of obj
+
+    Args:
+        obj: object to lookup
+
+    Returns:
+        a list of object
     """
     return (dir(obj))
