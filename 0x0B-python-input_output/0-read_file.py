ca9b7f563c8114468e4f3be8be407d9412d31456
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-02-06 03:36:51 -0800

    Task 0 - Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" file reader function """
+
 
 def read_file(filename=""):
     """
-    Function that reads file
+    Function that reads a .txt file
 
     Args:
         filename: name of file(txt) to read
+
     """
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end='')
