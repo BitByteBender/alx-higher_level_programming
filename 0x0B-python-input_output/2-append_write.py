ba96a8f8687269332dc6557fa8235f4d2d2405f6
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-02-06 03:57:38 -0800

    Task 2 - Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,14 +8,14 @@ def append_write(filename="", text=""):
     and returns a number count of chars added
 
     Args:
-        filename: text file to write into
-        text: string to count its chars
+        filename: text file to write/append into
+        text: string to append to file
 
     Notes:
         creates if file and append if it doesn't exist
 
     Returns:
-        chars count in string
+        number of chars added to file
     """
     with open(filename, mode='a', encoding="utf-8") as file:
         return (file.write(text))
