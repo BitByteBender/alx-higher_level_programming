769e56532dfcfaccc3ace6db3b8ddb22a83853d4
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-02-06 06:39:19 -0800

    Task 10 - Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student:
+    """ """
+    def __init__(self, first_name, last_name, age):
+        """
+        Init a student instance with first, last name & age
+        Args:
+            first_name: student first name
+            last_name: student last name
+            age: student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dict represnetation of student instance
+
+        Args:
+            attrs: list of attributes
+
+        Returns:
+            dict representation
+        """
+        if (attrs is not None):
+            res = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    res[attr] = getattr(self, attr)
+            return (res)
+        else:
+            return (self.__dict__)
