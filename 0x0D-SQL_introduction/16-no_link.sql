aa9bb3d985a7c510c069c880712ee27c5700a043
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-12 17:24:32 -0700

    Task 16 - say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all recs of second_table
-SELECT score, name FROM second_table ORDER BY score DESC, name DESC;
+SELECT score, name FROM second_table WHERE name != '' ORDER BY score DESC, name DESC;
