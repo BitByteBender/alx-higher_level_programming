37505e1fc98334f8a851950bcb19371b5f8e932b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-13 15:49:56 -0700

    Task 6 - States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
 -- creates hbtn_0d_usa db
-CREATE DATABASE IF NOT EXISTS "hbtn_0d_usa";
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- creates states table with a auto, unique, primary id
 CREATE TABLE IF NOT EXISTS states (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    UNIQUE (id)
+    PRIMARY KEY(id)
 );
