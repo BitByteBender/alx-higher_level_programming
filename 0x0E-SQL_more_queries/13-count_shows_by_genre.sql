f6db96acf22f0fde4a2abb2bbac7178c07b8a6e4
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-13 16:37:04 -0700

    Task 13 - Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name As genre, count(tv_show_genres.show_id) As number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_of_shows ASC;
+ORDER BY number_of_shows DESC;
