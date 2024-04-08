8c6042ba7ead99ff95df1f11f4b668a3020f7689
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-08 15:00:33 -0700

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+'use strict';
+
+const arrLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+while (i < arrLines.length) {
+  console.log(arrLines[i]);
+  i++;
+}
