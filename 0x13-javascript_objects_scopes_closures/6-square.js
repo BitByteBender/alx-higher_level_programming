0eff353d20cfe3269ba7c9f24bcc0c75c6d06a3f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-09 12:38:05 -0700

    Task 6 - Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+'use strict';
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let i = 0;
+    while (i < this.height) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'C';
+      }
+      console.log(row);
+      i++;
+    }
+  }
+}
+
+module.exports = Square;
