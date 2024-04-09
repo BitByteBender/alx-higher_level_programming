27c227c43198b1535cd74b428c9f5479e8393ec3
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-09 12:55:12 -0700

    Task 7 - Occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+'use strict';
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      counter++;
+    }
+  }
+  return (counter);
+};
