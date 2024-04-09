c9e8b8c789d98e7213959cf1a91f6be4b9ca0139
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-08 17:32:40 -0700

    Task 17 - Increment obj

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+'use strict';
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
