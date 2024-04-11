cf245d3894ac8d4c60eca2750c0945a3ccf3b071
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-10 23:35:35 -0700

    Task 1 - filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Script that filters all states from hbtn_0e_0_usa database """
+import MySQLdb
+import sys
+
+
+def display_states(usrname, password, db_name):
+    conn = MySQLdb.connect(host="localhost", port=3306, user=usrname,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    qry = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    cur.execute(qry)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if (__name__ == "__main__"):
+    display_states(sys.argv[1], sys.argv[2], sys.argv[3])
