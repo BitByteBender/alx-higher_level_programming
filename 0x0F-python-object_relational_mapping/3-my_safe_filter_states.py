fab57b9cd071fbb22e43c1dc444245119541b3d8
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-11 01:40:27 -0700

    Task 3 - SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that filters all states from hbtn_0e_0_usa database """
+import MySQLdb
+import sys
+
+
+def display_states(usrname, password, db_name, st_name):
+    conn = MySQLdb.connect(host="localhost", port=3306, user=usrname,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    qry = "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    cur.execute(qry.format(st_name))
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+
+if (__name__ == "__main__"):
+    for i in range(len(sys.argv[4])):
+        if sys.argv[4][i] == ';':
+            print("Error! Injections attempt")
+            sys.exit(1)
+
+    display_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
