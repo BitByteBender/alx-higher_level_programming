ec9173e44a9d5d1d7200e14ed25005f703fbaf9a
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-12 08:19:50 -0700

    Delete, update, add states

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script to display all states objs from hbtn_0e_6_usa database """
+""" Script that updates states objs by id from hbtn_0e_6_usa database """
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def display_states(usrname, password, db_name):
+def update_states(usrname, password, db_name):
     db_uri = f"mysql+mysqldb://{usrname}:{password}@localhost:3306/{db_name}"
     engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
@@ -21,4 +21,4 @@ def display_states(usrname, password, db_name):
 
 
 if (__name__ == "__main__"):
-    display_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    update_states(sys.argv[1], sys.argv[2], sys.argv[3])
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-""" Script that adds a new state name to hbtn_0e_6_usa database """
+""" Script that deletes states objs from hbtn_0e_6_usa that contains 'a' """
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def push_states(usrname, password, db_name):
+def deletes_states(usrname, password, db_name):
     db_uri = f"mysql+mysqldb://{usrname}:{password}@localhost:3306/{db_name}"
     engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
 
     session_up = session()
-    session_up.add(State(name='Louisiana'))
-    session_up.commit()
 
-    fetchInstance = session_up.query(State).filter_by(name='Louisiana').first()
-    print(fetchInstance.id)
+    fetchInstance = session_up.query(State).filter(State.name.like('%a%'))
+
+    for i in fetchInstance:
+        session_up.delete(i)
+
+    session_up.commit()
     session_up.close()
 
 
 if (__name__ == "__main__"):
-    push_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    deletes_states(sys.argv[1], sys.argv[2], sys.argv[3])
