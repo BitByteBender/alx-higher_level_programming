beb46be2d96d17d65d059c93028911937ba3c8a9
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-12 08:21:27 -0700

    Task 10 - Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script to display all states objs from hbtn_0e_6_usa database """
+""" Script get a state objs from hbtn_0e_6_usa database """
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
 
-def display_states(usrname, password, db_name, st_fetch):
+def get_states(usrname, password, db_name, st_fetch):
     db_uri = f"mysql+mysqldb://{usrname}:{password}@localhost:3306/{db_name}"
     engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
@@ -20,4 +20,4 @@ def display_states(usrname, password, db_name, st_fetch):
 
 
 if (__name__ == "__main__"):
-    display_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    get_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
