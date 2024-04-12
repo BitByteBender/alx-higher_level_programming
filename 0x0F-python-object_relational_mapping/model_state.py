4469c85338bbb4cd03474069f91f6d8d788c0d51
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-12 06:36:51 -0700

    Task 6 - first state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" First state model: Class State that inherits from Base """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,6 +7,7 @@ Base = declarative_base(metadata=MetaData())
 
 
 class State(Base):
+    """ Defining  Attributes """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
