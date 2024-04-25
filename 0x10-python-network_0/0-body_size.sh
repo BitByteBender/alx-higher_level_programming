cd5b9f6f0ccd7ead2c6ddc6349b39c4957d67844
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-25 04:58:54 -0700

    Task 0 - cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # cURL body size :> takes in a URL, sends a req to that URL & displays size of body of the response
 url="http://$1"
 res=$(curl -sI "$url" | grep -i Content-Length | wc -c)
