b716fdb130a65916b4db0355c92dfb3e65c88950
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-26 09:21:58 -0700

    Task 0 - What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import urllib.request
+import urllib.error
+
+
+def get_url(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            src = response.headers.get_content_type()
+            cnt = response.read()
+            utf8_cnt = cnt.decode("utf-8")
+
+            print("Body response:\n\t- type:", type(cnt))
+            print("\t- content:", cnt)
+            print("\t- utf8 content:", utf8_cnt)
+    except urllib.error.URLError as Err:
+        print("Error fetching URL:", Err)
+
+
+if (__name__ == "__main__"):
+    url = "https://alx-intranet.hbtn.io/status"
+    get_url(url)
