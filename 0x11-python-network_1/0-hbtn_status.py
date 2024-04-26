68c10a309b718eecc2edf6f4bf72210dae3bb390
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-26 09:45:47 -0700

    Task 0 - updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """ Script that fetches a given URL & display a body response """
 import urllib.request
-import urllib.error
 
 
 def get_url(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            src = response.headers.get_content_type()
-            cnt = response.read()
-            utf8_cnt = cnt.decode("utf-8")
+    with urllib.request.urlopen(url) as response:
+        src = response.headers.get_content_type()
+        cnt = response.read()
+        utf8_cnt = cnt.decode("utf-8")
 
-            print("Body response:\n\t- type:", type(cnt))
-            print("\t- content:", cnt)
-            print("\t- utf8 content:", utf8_cnt)
-    except urllib.error.URLError as Err:
-        print("Error fetching URL:", Err)
+        print("Body response:\n\t- type:", type(cnt))
+        print("\t- content:", cnt)
+        print("\t- utf8 content:", utf8_cnt)
 
 
 if (__name__ == "__main__"):
