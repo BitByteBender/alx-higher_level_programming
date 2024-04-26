5500b308c86cdf9384347021f4ed2dfaaed92a71
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-04-26 09:34:32 -0700

    Task 1 - Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Script that takes in a URL & send a req to it
+    & displays val of Var:X-Request-Id
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+def get_url(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            src = response.headers.get("X-Request-Id")
+            if src:
+                print(src)
+            else:
+                print("X-Request-Id not found")
+    except urllib.error.URLError as Err:
+        print("Error fetching URL:", Err)
+
+
+if (__name__ == "__main__"):
+    url = sys.argv[1]
+    get_url(url)
