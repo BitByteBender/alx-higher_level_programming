44d95fa15b760a2dca9640614f93a121a3ff788f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-21 08:32:14 -0700

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Script that prints title of a Star Wars movie via its ID
+'use_strict';
+
+const req = require('request');
+
+const mvID = process.argv[2];
+
+const urlEndpoint = `https://swapi-api.alx-tools.com/api/films/${mvID}`;
+
+req.get(urlEndpoint, (err, res, cnt) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(cnt).title);
+  }
+});
