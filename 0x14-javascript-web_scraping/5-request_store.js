47fca01413218f770366963f014895792fa4ad0f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-21 09:14:07 -0700

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Script that gets the contents of a webpage and stores it in a file.
+'use_strict';
+
+const req = require('request');
+const fsys = require('fs');
+
+const urlAPI = process.argv[2];
+const fPath = process.argv[3];
+
+req(urlAPI, (err, res, cnt) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  fsys.writeFile(fPath, cnt, 'utf-8', () => {});
+});
