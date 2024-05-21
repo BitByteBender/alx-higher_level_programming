4bf80e370d5ccbe8a61f5ea4ba95a339b12a8286
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-21 16:16:11 -0700

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// Script that prints all actors of Star Wars movie
+'use strict';
+
+const req = require('request');
+
+const mvID = process.argv[2];
+
+const urlEndpoint = `https://swapi-api.alx-tools.com/api/films/${mvID}`;
+
+req.get(urlEndpoint, (err, res, cnt) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const mv = JSON.parse(cnt); const chr = mv.characters;
+
+  chr.forEach((characterUrl) => {
+    req(characterUrl, (err, res, cnt) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log(JSON.parse(cnt).name);
+    });
+  });
+});
