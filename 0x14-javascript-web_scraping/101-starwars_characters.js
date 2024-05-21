0ffab95b2063305860b66da10b7046a34d5239d2
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-21 16:36:06 -0700

    Test -> Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+// Script that prints all actors of Star Wars movie
+'use strict';
+
+const req = require('request');
+
+const mvID = process.argv[2];
+
+const urlEndpoint = `https://swapi-api.alx-tools.com/api/films/${mvID}`;
+
+req.get(urlEndpoint, (err, res, cnt) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const mv = JSON.parse(cnt); const chr = mv.characters;
+
+  const retrieveCharName = (characterUrl) => {
+    req(characterUrl, (err, res, cnt) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log(JSON.parse(cnt).name);
+    });
+  };
+
+  const procChr = async () => {
+    for (const characterUrl of chr) {
+      await new Promise(resolve => {
+        retrieveCharName(characterUrl);
+        resolve();
+      });
+    }
+  };
+
+  procChr();
+});
