9416d9217a8834db77a96ab66671b953c8e3a135
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-21 08:05:56 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script that prints the status code of a url
+'use_strict';
+
+const req = require('request');
+
+const urlArg = process.argv[2];
+
+req.get(urlArg, (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
