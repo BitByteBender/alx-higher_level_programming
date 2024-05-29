5b3c4117215a78bf7ddf58f8c1c971bbbda62c6f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-28 23:20:16 -0700

    Add red class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,9 @@
+'use strict';
+/*
+ * Script that adds the class red to the header element
+ * when user clicks on DIV#red_header tag
+ * */
+
+$('div#red_header').click(function () {
+	$('header').addclass('red');
+});
