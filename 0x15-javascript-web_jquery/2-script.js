68c0819b9a2582c29d7071238135b0dc8cdeffc4
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-28 23:17:50 -0700

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,9 @@
+'use strict';
+/*
+ * Script that updates text color of the header to red
+ * when user clicks on DIV#red_header tag
+ * */
+
+$('div#red_header').click(function () {
+	$('header').css('color', '#FF0000');
+});
